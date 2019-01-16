ef2f0ae85c86372b80a212c7c9a399771627eabf
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-15 22:09:06 -0500

    file upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            Home Run Derby Lander
+        </title>
+    </head>
+    <body>
+        <h1>Choose your year</h1>
+        <ul>
+            <li><a href="../2015">2015</a></li>
+            <li><a href="../2016">2016</a></li>
+            <li><a href="../2017">2017</a></li>
+            <li><a href="../2018">2018</a></li>
+        </ul>
+    </body>
+</html>
