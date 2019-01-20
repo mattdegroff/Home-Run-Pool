03ca09e6ebc58f6f72a9d47a7bec81c211555055
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-19 20:28:13 -0500

    file restructure

--- index.php
+++ index.php
@@ -202,7 +202,7 @@
 		</div>
 	</nav>
 
-		<div class="tab-content">
+		<!--<div class="tab-content">
 	  	<div class="tab-pane fade show container active text-center" id="2018totals">Loading...</div>
 			<div class="tab-pane fade container text-center" id="2018picks">Loading...</div>
 	  	<div class="tab-pane fade container text-center" id="2017totals">Loading...</div>
@@ -211,7 +211,7 @@
   		<div class="tab-pane fade container text-center" id="2016picks">Loading...</div>
   		<div class="tab-pane fade container text-center" id="2015totals">Loading...</div>
   		<div class="tab-pane fade container text-center" id="2015picks">Loading...</div>
-		</div>
+		</div>-->
 
 		<section class="footer text-center" style="padding: 25px; margin-top: 10px; background-color: #eeeeee;">Created by Matt DeGroff</section>
 	</body>
