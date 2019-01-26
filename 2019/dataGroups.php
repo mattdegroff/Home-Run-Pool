089634984a2cd65b68c8ac6c37e129ecabadfaff
Matthew DeGroff <matthewdegroff@Matthews-MBP.fios-router.home>
2019-01-26 12:03:57 -0500

    basic rollout

--- 2019/dataGroups.php
+++ 2019/dataGroups.php
@@ -26,15 +26,24 @@
 		$deadline = new DateTime('2017-04-02T13:10:00', new DateTimeZone('America/New_York'));
 		$groupDead = new DateTime('2017-03-01T12:00:00', new DateTimeZone('America/New_York'));
 		$final = new DateTime('2017-10-01T23:59:59', new DateTimeZone('America/New_York'));
-	} else {
+	} else if (substr($_GET["id"],0,4) == "2018"){
 		include_once("connect2018.php");
 		$colorA = '#00cc66';
 		$colorB = '#d9d9d9';
 		$colorC = '#00cc66';
 		$colorD = '#d9d9d9';
-		$deadline = new DateTime('2018-02-28T12:40:00', new DateTimeZone('America/New_York'));
+		$deadline = new DateTime('2018-03-29T12:40:00', new DateTimeZone('America/New_York'));
 		$groupDead = new DateTime('2018-02-12T12:00:00', new DateTimeZone('America/New_York'));
 		$final = new DateTime('2018-09-30T23:59:59', new DateTimeZone('America/New_York'));
+	} else {
+		include_once("connect.php");
+		$colorA = '#92d050';
+		$colorB = '#00b0f0';
+		$colorC = '#ffd966';
+		$colorD = '#00b050';
+		$deadline = new DateTime('2019-03-28T13:05:00', new DateTimeZone('America/New_York'));
+		$groupDead = new DateTime('2018-02-21T12:00:00', new DateTimeZone('America/New_York'));
+		$final = new DateTime('2018-09-29T23:59:59', new DateTimeZone('America/New_York'));
 	}
 
 		include "update1.php";
