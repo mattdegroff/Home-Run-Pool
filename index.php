1e593d7d4bbcc6ef0b6e48c465f6f5ea2db797a5
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-26 22:29:37 -0500

    2019

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
     $version = 2019;
     //header('Location: 2019/index.php');
-    $db = parse_url(getenv("DATABASE_URL"));
+    $db = parse_url(getenv("HEROKU_POSTGRESQL_BLUE_URL"));
     $db["path"] = ltrim($db["path"], "/");
 
-    if ($conn = pg_connect(getenv("DATABASE_URL"))) {
+    if ($conn = pg_connect(getenv("HEROKU_POSTGRESQL_BLUE_URL"))) {
         echo "Connection Successful<br>"; 
     } else {
         echo "connection failed";
