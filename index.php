0b800530cdf4ea8d3d9010716d7b3f1ef42c1602
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-26 22:12:35 -0500

    2019

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
     $version = 2019;
-    header('Location: 2019/index.php');
+    //header('Location: 2019/index.php');
+    $db = parse_url(getenv("DATABASE_URL"));
+    $db["path"] = ltrim($db["path"], "/");
+
+    if ($conn = pg_connect(getenv("DATABASE_URL"))) {
+        echo "Connection Successful"; 
+    } else {
+        echo "connection failed";
+    }
 ?>
