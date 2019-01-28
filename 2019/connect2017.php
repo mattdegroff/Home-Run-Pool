8ff938f77eab92cbf0569775d69eba27b116007d
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-27 21:32:58 -0500

    rollout

--- 2019/connect2017.php
+++ 2019/connect2017.php
@@ -1,15 +1,16 @@
 <?php
-	$host_name  = "166.62.75.228";
-	$database   = "2017";
-	$user_name  = "mh37tmo638xn";
-	$password   = "#UVMI3Kr4";
+	$url = parse_url(getenv("CLEARDB_GOLD_URL"));
 
-	$conn = mysqli_connect($host_name, $user_name, $password, $database);
-
-	if ($conn->connect_error) {
-		echo '<script>console.log("Connection failed: "' . $conn->connect_error . '");</script>';
-	}
-	else {
-		echo '<script>console.log("Connection Established");</script>';
-	}
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+	
+	$conn = new mysqli($server, $username, $password, $db);
+		if ($conn->connect_error) {
+			echo '<script>console.log("Connection failed: "' . $conn->connect_error . '");</script>';
+		}
+		else {
+			echo '<script>console.log("Connection Established");</script>';
+		}
 ?>
--- 2019/connect2018.php
+++ 2019/connect2018.php
@@ -1,15 +1,16 @@
 <?php
-	$host_name  = "166.62.75.228";
-	$database   = "2018";
-	$user_name  = "mh37tmo638xn";
-	$password   = "#UVMI3Kr4";
+	$url = parse_url(getenv("CLEARDB_ORANGE_URL"));
 
-	$conn = mysqli_connect($host_name, $user_name, $password, $database);
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 	
-	if ($conn->connect_error) {
-		echo '<script>console.log("Connection failed: "' . $conn->connect_error . '");</script>';
-	}
-	else {
-		echo '<script>console.log("Connection Established");</script>';
-	}
+	$conn = new mysqli($server, $username, $password, $db);
+		if ($conn->connect_error) {
+			echo '<script>console.log("Connection failed: "' . $conn->connect_error . '");</script>';
+		}
+		else {
+			echo '<script>console.log("Connection Established");</script>';
+		}
 ?>
