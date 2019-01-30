c694c63e1e731dbebd4f6bfcf39cc9f117f218ad
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-30 17:48:13 -0500

    rollout

--- 2019/update1.php
+++ 2019/update1.php
@@ -421,6 +421,7 @@
 									if ($result->num_rows > 0) {
 										while($row = $result->fetch_assoc()){
 											$max = $row['max(id)'];
+											echo $max;
 										}
 									}
 									echo '<div class="row">';
