e101715494249243bb52cd5c76dbceaa032988ab
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-30 17:10:40 -0500

    rollout

--- 2019/dataGroups.php
+++ 2019/dataGroups.php
@@ -339,10 +339,10 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-lg-12">
+					<!--<div class="col-lg-12">-->
 			<?php
 					printGroups($deadline);
 				?>
-			</div>
+			<!--</div>-->
 			</div>
 		</div>
