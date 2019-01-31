02c7c9921ca809d09e600d9b4fc5164b4024f065
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-30 19:32:31 -0500

    rollout

--- 2019/topRowBefore.php
+++ 2019/topRowBefore.php
@@ -1,14 +1,26 @@
 <div class="card-group">
 		<div class="card" style=<?php echo '"background-color: ' . $colorA . '"'; ?>>
-			<img class="card-img-top img-fluid" src="<?php echo Apic(); ?>" alt="Card image">
+			<!--<img class="card-img-top img-fluid" src="<?php //echo Apic(); ?>" alt="Card image">-->
 			<div class="card-body">
-				<div class="card-title text-center"><b>A</b></div>
+				<div class="card-title text-center"><b>C</b></div>
 				<div class="row">
 					<div class="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9 text-center">Player</div>
-					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">%</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">HR</div>
 				</div>
 				<?php
-				$sql = "SELECT player, homeRuns FROM groupA ORDER BY homeRuns DESC LIMIT 1";
+                $sql = "select player, team, homeRuns, link from groupA order by id";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()){
+						echo '<div class="row">
+									<div class="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9 text-center">
+									<a href="'.$row['link'].'" style="color:black" target="_blank">'.$row['player']
+									.' ('.$row['team'].')</a>
+									</div>
+									<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">'.$row['homeRuns'].'</div></div>';
+							}
+					}
+				/*$sql = "SELECT player, homeRuns FROM groupA ORDER BY homeRuns DESC LIMIT 1";
 							$result = $conn->query($sql);
 							if ($result->num_rows > 0) {
 								while($row = $result->fetch_assoc()){
@@ -35,29 +47,33 @@
 									<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">'.$row['picks'].'</div></div>';
 							}
 					}
-				}
-
-				$sql = "select sum(picks) from groupA";
-			$result = $conn->query($sql);
-			if ($result->num_rows > 0) {
-				while($row = $result->fetch_assoc()){
-					echo '<div class="row"><div class="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9 text-center"></div><div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center"><b>'.$row['sum(picks)']."</b></div></div>";
-				}
-			}
+                }*/
 
 						?>
 			</div>
 		</div>
 		<div class="card" style=<?php echo '"background-color: ' . $colorB . '"'; ?>>
-			<img class="card-img-top img-fluid" src="<?php echo Bpic(); ?>" alt="Card image">
+			<!--<img class="card-img-top img-fluid" src="<?php //echo Bpic(); ?>" alt="Card image">-->
 			<div class="card-body">
-				<div class="card-title text-center"><b>B</b></div>
+				<div class="card-title text-center"><b>C</b></div>
 				<div class="row">
 					<div class="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9 text-center">Player</div>
-					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">%</div>
+					<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">HR</div>
 				</div>
 				<?php
-				$sql = "SELECT player, homeRuns FROM groupB ORDER BY homeRuns DESC LIMIT 1";
+                $sql = "select player, team, homeRuns, link from groupB order by id";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()){
+						echo '<div class="row">
+									<div class="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9 text-center">
+									<a href="'.$row['link'].'" style="color:black" target="_blank">'.$row['player']
+									.' ('.$row['team'].')</a>
+									</div>
+									<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">'.$row['homeRuns'].'</div></div>';
+							}
+					}
+				/*$sql = "SELECT player, homeRuns FROM groupB ORDER BY homeRuns DESC LIMIT 1";
 							$result = $conn->query($sql);
 							if ($result->num_rows > 0) {
 								while($row = $result->fetch_assoc()){
@@ -84,12 +100,12 @@
 									<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-center">'.$row['picks'].'</div></div>';
 							}
 					}
-				}
+				}*/
 						?>
 			</div>
 		</div>
 		<div class="card" style=<?php echo '"background-color: ' . $colorC . '"'; ?>>
-			<!--<img class="card-img-top img-fluid" src="<?php echo Cpic(); ?>" alt="Card image">-->
+			<!--<img class="card-img-top img-fluid" src="<?php //echo Cpic(); ?>" alt="Card image">-->
 			<div class="card-body">
 				<div class="card-title text-center"><b>C</b></div>
 				<div class="row">
