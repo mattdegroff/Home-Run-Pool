97505ae088e5c124f3c7800169b85c3bdf90b64e
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-01-30 19:02:28 -0500

    rollout

--- 2019/dataGroups.php
+++ 2019/dataGroups.php
@@ -53,7 +53,7 @@
 	<?php
 		if (new DateTime() < $deadline) {
 			if (new DateTime() < $groupDead) {
-				echo '<div class="alert alert-success alert-dismissable text-center fade show"><button type="button" class="close" data-dismiss="alert">&times;</button>Groups will be released on <br><strong>'.$groupDead->format('F j, Y g:i A').'</strong></div>';
+				echo '<div class="alert alert-success alert-dismissable text-center fade show"><button type="button" class="close" data-dismiss="alert">&times;</button>Groups will be released at<br><strong>'.$groupDead->format('g:i A').'</strong>on<strong>'.$groupDead->format('F j, Y').'</strong></div>';
 			} else {
 				echo '<div class="alert alert-success alert-dismissable text-center fade show"><button type="button" class="close" data-dismiss="alert">&times;</button>Teams lock <br><strong>'.$deadline->format('F j, Y g:i A').'</strong></div>';
 			}
