26636086f1bd05ef3cf40128c329b108a586accb
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-02-03 00:47:54 -0500

    database updates

--- 2019/index.php
+++ 2019/index.php
@@ -36,7 +36,7 @@
 				}
 				xmlhttp.send();
 			}
-				update("2019totals");
+				//update("2019totals");
 		</script>
 	</head>
 	<body onload="updateClock(); setInterval('updateClock()', 1000 )">
@@ -208,7 +208,7 @@
 		</div>
 	</nav>
 		<div id="main" class="container">
-				Loading...
+				<?php include("dataGroups.php?id=2019totals");
 		</div>
 		<section class="footer text-center" style="padding: 25px; margin-top: 10px; background-color: #eee;">Matt DeGroff &copy; <?php echo date("Y"); ?></section>
 	</body>
