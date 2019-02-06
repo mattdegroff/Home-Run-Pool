a01a5bb2d0758318a9bd1e8f00155788bb7288f0
Matthew DeGroff <matthewdegroff@Matthews-MacBook-Pro.local>
2019-02-06 11:38:00 -0500

    database updates

--- 2019/connect.php
+++ 2019/connect.php
@@ -1,15 +1,15 @@
 <?php
 $year = $_GET['year'];
 
-if ($year == "2015") {
+if ($year == 2015) {
 	$url = "CLEARDB_DATABASE_URL";
-} else if ($year == "2016") {
+} else if ($year == 2016) {
 	$url = "CLEARDB_CHARCOAL_URL";
-} else if ($year == "2017") {
+} else if ($year == 2017) {
 	$url = "CLEARDB_GOLD_URL";
-} else if ($year == "2018") {
+} else if ($year == 2018) {
 	$url = "CLEARDB_ORANGE_URL";
-} else if ($year == "2019") {
+} else if ($year == 2019) {
 	$url = "CLEARDB_JADE_URL";
 }
 
