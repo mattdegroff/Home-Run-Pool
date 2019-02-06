2d1d7474cf16b02e451ff995a23e58a36d91da9a
Matthew DeGroff <matthewdegroff@Matthews-MacBook-Pro.local>
2019-02-06 11:39:00 -0500

    database updates

--- 2019/connect.php
+++ 2019/connect.php
@@ -22,7 +22,7 @@ $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 	if ($conn->connect_error) {
-		echo '<script>console.log("Connection failed: "' . $conn->connect_error . '");</script>';
+		echo '<script>console.log("Connection failed: ' . $conn->connect_error . '");</script>';
 	}
 	else {
 		echo '<script>console.log("Connection Established");</script>';
