567227be0f3cf768c4897abe9ecd2d8eb28c8783
Matthew DeGroff <matthewdegroff@Matthews-MacBook-Pro.local>
2019-02-08 17:28:10 -0500

    database updates

--- new/delete.php
+++ new/delete.php
@@ -3,16 +3,16 @@ session_start();
 $_SESSION['year'] = 2019;
 include("connect.php");
 
-$sql = "select code, count(code) from standings";
-		$result = $conn->query($sql);
-			while($row = $result->fetch_assoc()){
-                echo $row['count(code)'];
-				$sql1 = "delete from ".$row['code'];
-		        if($result = $conn->query($sql1)) { 
-                    echo $row['code']." success";
-                }
-			}
-        echo "delete complete";
+    $sql = "select code, count(code) from standings";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()){
+        echo $row['count(code)'];
+        $sql1 = "delete from ".$row['code'];
+        if($result = $conn->query($sql1)) { 
+            echo $row['code']." success";
+        }
+    }
+    echo "delete complete";
 
         ?>
         
