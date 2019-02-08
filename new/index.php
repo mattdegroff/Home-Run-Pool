1fc980941495eb74cf82535e8705ad26ec3a18b6
Matthew DeGroff <matthewdegroff@Matthews-MacBook-Pro.local>
2019-02-08 15:10:30 -0500

    database updates

--- new/index.php
+++ new/index.php
@@ -2,7 +2,7 @@
 	session_start();
 	if(!isset($_SESSION['year']) && !isset($_SESSION['page'])) {
 		$_SESSION['year'] = 2019;
-		$_SESSION['page'] = "picks";
+		$_SESSION['page'] = "totals";
 	}
 	print_r($_SESSION);
 ?>
