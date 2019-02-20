95a0468668cfae85664d0250d7e428af225b750f
Matthew DeGroff <matthewdegroff@Matthews-MBP.fios-router.home>
2019-02-19 22:14:05 -0500

    tweak

--- 2019/cron/cronA.php
+++ 2019/cron/cronA.php
@@ -4,8 +4,10 @@
 	include "../update.php";
 	
 	$start = microtime(true);
+	$file = 'cronLog.txt';
 	A();
 	echo "A updated<br>";
+	file_put_contents($file, Date()." A updated", FILE_APPEND);
 	$end = microtime(true);
 	$time = $end-$start;
 	echo "time: ".$time;
