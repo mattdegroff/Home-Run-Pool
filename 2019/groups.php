db2ba085ab020ac419a2ad0472c91228f4038b42
Matthew DeGroff <matthewdegroff@Matthews-MacBook-Pro.local>
2019-02-21 09:27:25 -0500

    tweak

--- 2019/groups.php
+++ 2019/groups.php
@@ -44,12 +44,125 @@
 		}
 
 	</style>
-<?php
-include("details.php");
-include("update1.php");
+<table align=center>
+								<tbody>
+										<?php
 
-$deadline = new DateTime('2018-03-29T12:40:00', new DateTimeZone('America/New_York'));
+										include_once("details.php");
+										include_once("update1.php");
+
+										$deadline = new DateTime('2018-03-29T12:40:00', new DateTimeZone('America/New_York'));
 		
-printGroups($deadline);?>
+											echo "<tr>";
+											$sql = "select max(id) from standings";
+											$result = $conn->query($sql);
+											if ($result->num_rows > 0) {
+												while($row = $result->fetch_assoc()){
+													$maxId = $row['max(id)'];
+												}
+											}
+
+											for ($i =1; $i <= $maxId; $i++) {
+												$sql = "select color, name, font, code, paid from standings where id=".$i;
+												$result = $conn->query($sql);
+												if ($result->num_rows > 0) {
+													while($row = $result->fetch_assoc()){
+														$color = $row['color'];
+														$name = $row['name'];
+														$font = $row['font'];
+														$code = $row['code'];
+														$paid = $row['paid'];
+													}
+												}
+
+												echo "<td><table border = 6px bgcolor=".$color.">";
+												if ($font == 1) {
+													echo '<thead id="dark">';
+												} else if ($font == 2) {
+													echo '<thead id="yellow">';
+												} else if ($font == 3) {
+													echo '<thead id="red">';
+												} else {
+													echo "<thead>";
+												}
+												if (new DateTime() > $deadline) {
+													if ($paid == 1) {
+														echo "<tr><th>".$name."</th></tr></thead>";
+													} else {
+														echo "<tr><th>*".$name."</th></tr></thead>";
+													}
+												} else {
+													if ($paid == 1) {
+														echo "<tr><th>".$name."</th></tr></thead>";
+													} else {
+														echo "<tr><th>*".$name."</th></tr></thead>";
+													}
+												}
+												if ($font == 1) {
+													if (new DateTime() > $deadline) {
+														echo '<tbody><tr><td><table id="dark"><thead><tr>
+													<th width="150">Player</th><th>HR</th>
+													</tr></thead><tbody>';
+													}
+												} else if ($font == 2) {
+													if (new DateTime() > $deadline) {
+														echo '<tbody><tr><td><table id="yellow"><thead><tr>
+													<th width="150">Player</th><th>HR</th>
+													</tr></thead><tbody>';
+													}
+												} else if ($font == 3) {
+													if (new DateTime() > $deadline) {
+														echo '<tbody><tr><td><table id="red"><thead><tr>
+													<th width="150">Player</th><th>HR</th>
+													</tr></thead><tbody>';
+													}
+												} else {
+													if (new DateTime() > $deadline) {
+														echo '<tbody><tr><td><table><thead><tr>
+													<th width="150">Player</th><th>HR</th>
+													</tr></thead><tbody>';
+													}
+												}
+												if (new DateTime() > $deadline) {
+													printGroup($code);
+													echo "</tbody></table></td></tr></tbody></table></td>";
+												} else {
+													echo "</tbody></table></td>";
+												}
+												if (new DateTime() > $deadline) {
+													if ($i % 6 == 0) {
+														if ($i == $maxId) {
+															echo "</tr>";
+														} else {
+															echo "</tr><tr>";
+														}
+													} else {
+														if ($i == $maxId) {
+															echo "</tr>";
+														}
+													}
+												} else {
+													if ($i % 3 == 0) {
+														if ($i == $maxId) {
+															echo "</tr>";
+														} else {
+															echo "</tr><tr>";
+														}
+													} else {
+														if ($i == $maxId) {
+															echo "</tr>";
+														}
+													}
+												}
+											}
+										?>
+								</tbody>
+							</table>
+						</td>
+					</tr>
+				</table>
 											</body>
 										</html>
+<?php
+session_destroy();
+?>
