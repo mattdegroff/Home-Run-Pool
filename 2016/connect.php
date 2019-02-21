98d28d38fd708295080a50b407229ac78084e214
Matthew DeGroff <matthewdegroff@Matthews-MacBook-Pro.local>
2019-02-21 13:52:42 -0500

    tweak

--- 2016/connect.php
+++ 2016/connect.php
@@ -1,5 +1,20 @@
 <?php	
-	$host_name  = "166.62.75.228";
+$connect_url = "CLEARDB_CHARCOAL_URL";
+$url = parse_url(getenv($connect_url));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+	if ($conn->connect_error) {
+		echo '<script>console.log("Connection failed: ' . $conn->connect_error . '");</script>';
+	}
+	else {
+		echo '<script>console.log("Connection Established");</script>';
+	}
+	/*$host_name  = "166.62.75.228";
 	$database   = "2016";
 	$user_name  = "mh37tmo638xn";
 	$password   = "#UVMI3Kr4";
@@ -11,5 +26,5 @@
 	}
 	else {
 		echo '<script>console.log("Connection Established");</script>';
-	}
+	}*/
 ?>
