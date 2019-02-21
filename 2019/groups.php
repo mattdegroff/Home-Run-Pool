4f3925f6a8919ae9387dba2978faee03b611a08f
Matthew DeGroff <matthewdegroff@Matthews-MacBook-Pro.local>
2019-02-21 13:35:12 -0500

    tweak

--- 2019/groups.php
+++ 2019/groups.php
@@ -65,68 +65,6 @@
 	include("update1.php");
 
 	$deadline = new DateTime('2018-03-29T12:40:00', new DateTimeZone('America/New_York'));
-	
-	function printGroups($deadline) {
-		global $conn;
-		$sql = "select max(id) FROM standings";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()){
-				$max = $row['max(id)'];
-			}
-		}
-			echo '<div class="row">';
-
-			for ($i = 1; $i <= $max; $i++) {
-				echo "<div class='col-lg-3'>";
-				$sql = "select color, name, code, font, paid FROM standings where id=".$i;
-				$result = $conn->query($sql);
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()){
-						if ($row['color'] == 'white') {
-							echo '<div class="card" style="border-color: 1px solid rgba(0, 0, 0, 0.2)">';
-						} else {
-							echo '<div class="card" style="border-color: '.$row['color'].'">';
-						}
-						if ($row['font'] == 1) {
-							echo '<div class="card-header text-center" id="dark" style="background-color: '.$row['color'].'">';
-						} else if ($row['font'] == 2) {
-							echo '<div class="card-header text-center" id="yellow" style="background-color: '.$row['color'].'">';
-						} else if ($row['font'] == 3) {
-							echo '<div class="card-header text-center" id="red" style="background-color: '.$row['color'].'">';
-						} else {
-							if ($row['color'] == 'white') {
-								echo '<div class="card-header text-center" style="background-color: white; border-bottom: 1px solid rgba(0, 0, 0, 0.2);">';
-							} else {
-								echo '<div class="card-header text-center" style="background-color: '.$row['color'].'">';
-							}
-						}
-						if ($row['paid'] == 1) {
-							echo '<h5>'.$row['name'].'</h5></div>';
-						} else {
-							echo '<h5>*'.$row['name'].'*</h5></div>';
-						}
-							echo "</div>";
-					}
-				}
-
-				if ($i % 4 == 0) {
-					if ($i == $max) {
-						echo "</div>";
-					} else {
-						echo '</div><div class="row">';
-					}
-				} else {
-					if ($i == $max) {
-						$add = 4-($i%4);
-						for ($j=1; $j <= $add; $j++) {
-							echo '<div class="card" style="visibility: hidden;"></div>';
-						}
-						echo "</div>";
-					}
-				}
-			}
-		}
 	?>
 	<div class="row">
 		<div class="container" style="margin-top: 15px;">
