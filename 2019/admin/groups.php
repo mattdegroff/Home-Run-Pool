1322c1a9c67214909e438b0dda2974d65ead268b
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-03-23 19:47:07 -0400

    test

--- 2019/admin/groups.php
+++ 2019/admin/groups.php
@@ -7,11 +7,11 @@
 <head>
 		<title>YABO '19</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="../css/bootstrap.css">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/clock.js"></script>
+		<script type="text/javascript" src="../js/clock.js"></script>
 		
 	</head>
 <body>
