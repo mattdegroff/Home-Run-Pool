b9c63f86723d0ab5c144f3e71f0a029ada11be3e
Matthew DeGroff <matthewdegroff@Matthews-MBP.fios-router.home>
2019-03-28 03:42:10 -0400

    test

--- 2019/admin/insert.php
+++ 2019/admin/insert.php
@@ -10,15 +10,15 @@
 
 	//next D id is 24
 
-	$person = "zippy";
-	$A = "Mike Trout";
-	$B = "Nolan Arenado";
-	$C1 = "Ronald Acu&#241;a Jr.";
-	$C2 = "Rhys Hoskins";
+	$person = "lark";
+	$A = "Aaron Judge";
+	$B = "Joey Gallo";
+	$C1 = "Rhys Hoskins";
+	$C2 = "Trevor Story";
 	$D1 = "Jesus Aguilar";
 	$D2 = "Cody Bellinger";
-	$D3 = "Michael Conforto";
-	$D4 = "Eloy Jimenez";
+	$D3 = "Matt Olson";
+	$D4 = "Eugenio Suarez";
 
 	if ($run) {
 	$sql = "insert into " . $person . " (id, player) values (1, '" . $A . "')";
@@ -104,7 +104,7 @@
 	} else {
 		echo "query failed: " . $conn->error . "<br>";
 	}
-*/
+
 	$sql = "insert into groupD (id, firstName, lastName, link) values (23, 'Eloy', 'Jimenez', '".playerSearch($D4)."')";
 	$result = $conn->query($sql);
 	if ($result == true) {
@@ -114,7 +114,7 @@
 	}
 
 	echo "<br> entered new D players<br>";
-
+*/
 
 	$sql = "update " . $person . ", groupA set " . $person . ".pid = groupA.id, " . $person . ".homeRuns = groupA.homeRuns where groupA.player = " . $person . ".player and " . $person . ".id = 1";
 	$result = $conn->query($sql);
