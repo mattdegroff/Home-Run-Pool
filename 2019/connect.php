59ead9d57ce230204e6448f6d349045f1671830c
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-03-30 19:27:22 -0400

    test

--- 2019/connect.php
+++ 2019/connect.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 $year = $_SESSION['year'];
 
 if ($year == 2015) {
--- 2019/details.php
+++ 2019/details.php
@@ -1,5 +1,4 @@
 <?php
-	session_start();
 	include("connect.php");
     if ($_SESSION['year'] == 2015){
 		$colorA = 'red';
