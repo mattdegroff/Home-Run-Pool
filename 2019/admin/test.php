765d9dea6ee90a4040bd474dc3bd2cf63a3ad7c0
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-03-30 18:24:26 -0400

    test

--- 2019/admin/test.php
+++ 2019/admin/test.php
@@ -1,18 +1,1 @@
-<html>
-	<head>
-	<title>Query string </title>
-	</head>
-	<body>
-
-	<?php
-		session_start();
-$_SESSION['year'] = 2019;
-		print_r($_SESSION);
-		include("details.php");
-		include("update1.php");
-		include("topRowAfter.php");
-		printGroups($deadline);
-	?>
-	
-	</body>
-	</html>
+<?php phpinfo(); ?>
