be2d06b0539745946dde2b07879c29b09e19521b
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-03-30 19:22:01 -0400

    test

--- 2019/index.php
+++ 2019/index.php
@@ -4,6 +4,9 @@
 		$_SESSION['year'] = 2019;
 		$_SESSION['page'] = "totals";
 	}
+	ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 	include("details.php");
 ?>
