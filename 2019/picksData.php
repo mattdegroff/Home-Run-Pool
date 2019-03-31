f352da6fb13e8229f896f63bced2b0f90d603dd3
Matthew DeGroff <matthewdegroff@Matthews-MBP.home>
2019-03-30 20:43:45 -0400

    test

--- 2019/picksData.php
+++ 2019/picksData.php
@@ -1,5 +1,10 @@
 <?php
 		session_start();
+
+		if(!isset($_SESSION['year']) && !isset($_SESSION['page'])) {
+			$_SESSION['year'] = 2019;
+			$_SESSION['page'] = "totals";
+		}
 		include("details.php");
 		include_once("update1.php");
 
